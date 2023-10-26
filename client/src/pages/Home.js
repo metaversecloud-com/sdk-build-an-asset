5bfc60193b5f5cb5b5ffa26cf40dfb657b770c94
Fabio Alexandrino <fabioalxk@users.noreply.github.com>
2023-10-26 18:04:36 -0300

    snowman

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,19 +1,41 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import mergeImages from "merge-images";
 import { ClipLoader } from "react-spinners";
 import "./Home.scss";
 
+const accessories = {
+  scarf: ["scarf_0.png", "scarf_1.png", "scarf_2.png"],
+  arms: ["arms_0.png", "arms_1.png", "arms_2.png"],
+  hat: ["hat_0.png", "hat_1.png", "hat_2.png"],
+};
+
 function StartAssetView() {
-  const dispatch = useDispatch();
   const [loading, setLoading] = useState(true);
+  const [selected, setSelected] = useState({ scarf: "", arms: "", hat: "" });
+  const [preview, setPreview] = useState("/assets/snowman/snowman.png");
 
   useEffect(() => {
-    const fetchDroppedAsset = async () => {
-      setLoading(false);
-    };
+    setLoading(false);
+  }, []);
 
-    fetchDroppedAsset();
-  }, [dispatch]);
+  const updateSnowman = (type, image) => {
+    // Crie uma cópia atualizada do estado "selected"
+    const updatedSelected = { ...selected, [type]: image };
+
+    setSelected(updatedSelected);
+
+    // Use "updatedSelected" para atualizar o snowman
+    const imagesToMerge = [
+      { src: "/assets/snowman/snowman.png", x: 0, y: 0 },
+      ...Object.values(updatedSelected).map((item) => ({
+        src: item,
+        x: 0,
+        y: 0,
+      })),
+    ].filter((img) => img.src);
+
+    mergeImages(imagesToMerge).then(setPreview);
+  };
 
   if (loading) {
     return (
@@ -23,11 +45,27 @@ function StartAssetView() {
     );
   }
 
-  function startScreen() {
-    return <></>;
-  }
-
-  return startScreen();
+  return (
+    <div className="wrapper">
+      <h1>Customize your Snowman!</h1>
+      <img src={preview} alt="Snowman Preview" />
+      {Object.keys(accessories).map((type) => (
+        <div key={type}>
+          <h2>Select {type}</h2>
+          <div>
+            {accessories[type].map((image) => (
+              <img
+                key={image}
+                src={`/assets/snowman/${image}`}
+                alt={image}
+                onClick={() => updateSnowman(type, `/assets/snowman/${image}`)}
+              />
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default StartAssetView;
