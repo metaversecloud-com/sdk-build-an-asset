d5366c0ae1b10f57feec58ee4d7c4335b3b32400
Fabio Alexandrino <fabioalxk@users.noreply.github.com>
2024-01-15 18:22:18 -0300

    version 8

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ import { fileURLToPath } from "url";
 checkEnvVariables();
 const PORT = process.env.PORT || 3000;
 const app = express();
-const version = "7.0";
+const version = "8.0";
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
