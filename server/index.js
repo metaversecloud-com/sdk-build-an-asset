1d325c6d83522eaffa0daa540e71787da7c38590
Fabio Alexandrino <fabioalxk@users.noreply.github.com>
2024-01-19 17:09:10 -0300

    update version

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ import { fileURLToPath } from "url";
 checkEnvVariables();
 const PORT = process.env.PORT || 3000;
 const app = express();
-const version = "8.0";
+const version = "9.0";
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
