b096a20b55b06a798c22806350d4c7387a49a249
Lina <linabell@mail.com>
2026-04-15 10:24:31 -0700

    fix broken build

--- server/controllers/handlePickupDroppedAsset.ts
+++ server/controllers/handlePickupDroppedAsset.ts
@@ -44,7 +44,8 @@ export const handlePickupDroppedAsset = async (req: Request, res: Response) => {
       if (Object.keys(droppedAssets).length > 0) {
         const droppedAssetIds: string[] = [];
         for (const index in droppedAssets) {
-          if (droppedAssets[index].id) droppedAssetIds.push(droppedAssets[index].id);
+          const id = droppedAssets[index].id;
+          if (id) droppedAssetIds.push(id);
         }
         await World.deleteDroppedAssets(
           credentials.urlSlug,
